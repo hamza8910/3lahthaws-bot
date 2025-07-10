a183cb76fda1d8b2e832fe8fae452cf1ebbbf251
Hhzjzizozi <alinealone73@gmail.com>
2025-07-10 03:39:46 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN || 'YOUR_BOT_TOKEN_HERE',
+  DEVELOPER_ID: process.env.DEVELOPER_ID || 'V_b_L_o',
+  GROUP_INVITE_LINK: 'https://t.me/+0ipdbPwuF304OWRk',
+  BOT_USERNAME: 'spy_spy_bbot',
+  
+  GAME_SETTINGS: {
+    MIN_PLAYERS: 3,
+    MAX_PLAYERS: 30,
+    MAX_SPIES: 10,
+    JOIN_TIMEOUT: 90000, // 1.5 دقيقة
+    VOTE_TIMEOUT: 40000, // 40 ثانية
+    MIN_GAME_TIME: 1,
+    MAX_GAME_TIME: 15
+  },
+  
+  IMAGES: {
+    WELCOME: 'https://pin.it/2qzWrzy',
+    SPY_ROLE: 'https://pin.it/2Xv5gZUHU'
+  },
+  
+  VIDEOS: {
+    SPY_WIN: 'https://raw.githubusercontent.com/hamza8910/3lahthaws-bot/main/assets/spy_win.mp4',
+    PLAYERS_WIN: 'https://raw.githubusercontent.com/hamza8910/3lahthaws-bot/main/assets/players_win.mp4'
+  },
+  
+  SHOP_ITEMS: {
+    'تبون': 100000000,
+    'شنڤريحة': 200000000,
+    'شاب بيلو': 300000000,
+    'ديدين كلاش': 400000000,
+    'ايناس عبدلي': 500000000,
+    'ريفكا': 600000000,
+    'كريم': 700000000,
+    'عادل ميكسيك': 800000000,
+    'مراد طهاري': 1500000000
+  },
+  
+  BANKS: ['بدر', 'الهلال', 'أويحي'],
+  
+  REWARDS: {
+    SPY_WIN: 2000000000,
+    PLAYER_WIN: 100000
+  }
+};
